88ee06affb201deb73ab2b706cbc339fe4acab10
nirpesh23 <58818163+nirpesh23@users.noreply.github.com>
2020-01-09 10:43:30 +0545

    editing errors

--- KeyLandSpace/Products/views.py
+++ KeyLandSpace/Products/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template,Context
+from .models import Products
 
 # Create your views here.
 def view_product_lists(request):
-    list_of_products= products.objects.all()
+    list_of_products= Products.objects.all()
     print(list_of_products)
     context_variable = {
         'products':list_of_products
     }
-    return render(request,'products/products.html',context_variable)                
+    return render(request,'products/products.html',context_variable)   
+                 
