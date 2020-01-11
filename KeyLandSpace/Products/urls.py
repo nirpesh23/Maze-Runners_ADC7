9130d7c5f7367864360c790b9cd2551c4cb7a3b9
kabita-stha <58837481+kabita-stha@users.noreply.github.com>
2020-01-11 13:30:10 +0545

    kabita

--- KeyLandSpace/Products/urls.py
+++ KeyLandSpace/Products/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('templates/ourprofile.html', view_our_profile),
+	path('templates/contactus.html', view_contact_us),
+	
 ]
--- KeyLandSpace/Products/views.py
+++ KeyLandSpace/Products/views.py
@@ -4,3 +4,8 @@ from django.template import Template,Context
 from .models import Product
 
 # Create your views here.
+def view_our_profile(request):
+	return render(request,'ourprofile.html')
+
+def view_contact_us(request):
+	return render(request,'contactus.html')
