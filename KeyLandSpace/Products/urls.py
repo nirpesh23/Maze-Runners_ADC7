b11a524ab4d1ddd3778eb30f1de8f48be5c8298e
kabita-stha <58837481+kabita-stha@users.noreply.github.com>
2020-01-11 14:41:22 +0545

    creating pages

--- KeyLandSpace/Products/urls.py
+++ KeyLandSpace/Products/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('templates/ourprofile.html', view_our_profile),
 	path('templates/contactus.html', view_contact_us),
-	
+	path('templates/catogories.html', view_catogories),
+	path('templates/sell.html', view_sell),
+	path('templates/buy.html', view_buy),
 ]
--- KeyLandSpace/Products/views.py
+++ KeyLandSpace/Products/views.py
@@ -9,3 +9,12 @@ def view_our_profile(request):
 
 def view_contact_us(request):
 	return render(request,'contactus.html')
+
+def view_catogories(request):
+	return render(request,'catogories.html')
+
+def view_sell(request):
+	return render(request,'sell.html')
+
+def view_buy(request):
+	return render(request,'buy.html')
