f25f7c5b7ef7c024bb84ebb97adf18c1a29ef557
Pratima121 <58822379+Pratima121@users.noreply.github.com>
2020-01-11 22:20:45 +0545

    update

--- KeyLandSpace/Products/urls.py
+++ KeyLandSpace/Products/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+     path('productdata/edit/<int:ID>',view_productdata_updateform),
+     path('productdata/edit/update/<int:ID>',view_update_form_data_in_db),
+
+     path('templates/homepage.html', view_homepage),
+	
+	 
 ]
